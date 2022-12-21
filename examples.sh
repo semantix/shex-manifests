c6f050c793e6b2dbe68f20d56a6508b3743a3d75
dksharma <sharma.deepak2@mayo.edu>
2022-12-21 15:49:22 -0600

    quantity shape test

--- examples.sh
+++ examples.sh
@@ -6,7 +6,9 @@
 #
 # examples to include in README.md
 
-declare -a examples=( "basic_type_test#A Basic Type Test"
+declare -a examples=(
+					 "basic_type_test#A Basic Type Test"
 	                 "ts1#for testing - a simple example"
 	                 "quantity_test#Quantity Resource Tests for type arcs"
+	                 "constraint_test#A collection of ShEx constraint tests"
 	                 )
