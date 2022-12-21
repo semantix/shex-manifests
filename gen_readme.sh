1d4c9c9c110a7a1526d2d1207fbd222a7a6a0fd8
dksharma <sharma.deepak2@mayo.edu>
2022-12-20 18:21:50 -0600

    Updated

--- gen_readme.sh
+++ gen_readme.sh
@@ -12,6 +12,7 @@ source $DIR/examples.sh
 FILE_NAME=README.md
 SHEX_VALIDATOR=http://shex.io/webapps/shex.js/doc/shex-simple?manifestURL=
 SHEX_MANIFEST=https://semantix.github.io/shex-manifests
+NEW_WINDOW={:target=\"_blank\" rel=\"noopener\"}
 
 echo "# ShEx-mainfests" > $FILE_NAME
 echo "Manifest tests and examples for FHIR ShEx" >> $FILE_NAME
@@ -23,7 +24,7 @@ echo "| ------- | ----------- |" >> $FILE_NAME
 for exmp in "${examples[@]}"
 do
 	IFS='#' read -r -a exmpstrs <<< "$exmp"
-	echo "|[${exmpstrs[0]}]($SHEX_VALIDATOR$SHEX_MANIFEST/${exmpstrs[0]}/${exmpstrs[0]}.yaml){:target=\"_blank\" rel=\"noopener\"}|${exmpstrs[1]}|" >> $FILE_NAME
+	echo "| [${exmpstrs[0]}]($SHEX_VALIDATOR$SHEX_MANIFEST/${exmpstrs[0]}/${exmpstrs[0]}.yaml) $NEW_WINDOW | ${exmpstrs[1]} |" >> $FILE_NAME
 done
 
 
