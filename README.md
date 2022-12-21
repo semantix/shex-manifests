e91678fc954e99334c4593cbea0d1b8ceb21e11e
dksharma <sharma.deepak2@mayo.edu>
2022-12-20 18:27:32 -0600

    Updated

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Manifest tests and examples for FHIR ShEx
 [Try it](fhircat.github.io/FHIR-ShEx-manifests/-fhir-constraints.yaml)
 | Example | Description |
 | ------- | ----------- |
-| [basic_type_test](http://shex.io/webapps/shex.js/doc/shex-simple?manifestURL=https://semantix.github.io/shex-manifests/basic_type_test/basic_type_test.yaml)  {:target="_blank" rel="noopener"} | A Basic Type Test |
-| [ts1](http://shex.io/webapps/shex.js/doc/shex-simple?manifestURL=https://semantix.github.io/shex-manifests/ts1/ts1.yaml)  {:target="_blank" rel="noopener"} | for testing - a simple example |
+{:target=_blank| A Basic Type Test |
+{:target=_blank| for testing - a simple example |
--- gen_readme.sh
+++ gen_readme.sh
@@ -12,7 +12,7 @@ source $DIR/examples.sh
 FILE_NAME=README.md
 SHEX_VALIDATOR=http://shex.io/webapps/shex.js/doc/shex-simple?manifestURL=
 SHEX_MANIFEST=https://semantix.github.io/shex-manifests
-NEW_WINDOW='{:target="_blank" rel="noopener"}'
+NEW_WINDOW={:target="_blank" rel="noopener"}
 
 echo "# ShEx-mainfests" > $FILE_NAME
 echo "Manifest tests and examples for FHIR ShEx" >> $FILE_NAME
@@ -24,7 +24,9 @@ echo "| ------- | ----------- |" >> $FILE_NAME
 for exmp in "${examples[@]}"
 do
 	IFS='#' read -r -a exmpstrs <<< "$exmp"
-	echo "| [${exmpstrs[0]}]($SHEX_VALIDATOR$SHEX_MANIFEST/${exmpstrs[0]}/${exmpstrs[0]}.yaml) " $NEW_WINDOW "| ${exmpstrs[1]} |" >> $FILE_NAME
+	LINK1="| [${exmpstrs[0]}]($SHEX_VALIDATOR$SHEX_MANIFEST/${exmpstrs[0]}/${exmpstrs[0]}.yaml) "
+	LINK2="| ${exmpstrs[1]} |"
+	echo  ${LINK}${NEW_WINDOW}${LINK2} >> $FILE_NAME
 done
 
 
