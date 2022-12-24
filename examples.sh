9816a55e8a5cc417b18240b3d8fe597b5bd9b1b0
dksharma <sharma.deepak2@mayo.edu>
2022-12-24 15:36:40 -0600

    quantity/age shape tests

--- examples.sh
+++ examples.sh
@@ -10,5 +10,6 @@ declare -a examples=(
 					 "basic_type_test#A Basic Type Test"
 	                 "ts1#for testing - a simple example"
 	                 "quantity_test#Quantity Resource Tests for type arcs"
+					 "constraint_tests1#A collection of ShEx constraint tests without IMPORT"
 	                 "constraint_tests#A collection of ShEx constraint tests"
 	                 )
